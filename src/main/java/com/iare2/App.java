00d920e22b500d94b33a12945543d0a7d50ec935
SAATHVIK7878 <saathvikkukkadapu@gmail.com>
2025-07-03 07:02:32 +0530

    first commit on calculator

---
+++ src/main/java/com/iare2/App.java
@@ -0,0 +1,14 @@
+package com.iare2;
+
+
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
+
---
+++ src/test/java/com/iare2/AppTest.java
@@ -0,0 +1,21 @@
+package com.iare2;
+
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    Calculator calc = new Calculator();
+
+    @Test
+    void testAdd() {
+        assertEquals(5, calc.add(2, 3));
+    }
+
+    @Test
+    void testSubtract() {
+        assertEquals(1, calc.subtract(3, 2));
+    }
+}
+
